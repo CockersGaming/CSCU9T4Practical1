f890abd746998a0727c767b02b5b3d86c6270998
James <jamescockfield10@gmail.com>
2021-02-06 00:20:02 +0000

    add setters

--- src/main/java/com/stir/cscu9t4practical1/Entry.java
+++ src/main/java/com/stir/cscu9t4practical1/Entry.java
@@ -4,10 +4,22 @@ package com.stir.cscu9t4practical1;
 import java.util.Calendar;
 public class Entry {
   private String name;
-  private String activity = "";
   private Calendar dateAndTime;
   private float distance;
-  
+  private String tempo, terrain, where;
+  private int repetitions, recovery;
+
+  /**
+   * This is the constructor for Entry.
+   * @param n is the name of the Entry
+   * @param d is the day of the Entry
+   * @param m is the month of the Entry
+   * @param y is the year of the Entry
+   * @param h is the hour of the Entry
+   * @param min is the minute of the Entry
+   * @param s is the second of the Entry
+   * @param dist is the distance of the Entry
+   */
   public Entry (String n, int d, int m, int y, int h, int min, int s, float dist) {
     name = n;
     Calendar inst = Calendar.getInstance();
@@ -19,9 +31,6 @@ public class Entry {
   public String getName () {
     return name;
   } //getName
-  public String getActivity () {
-    return activity;
-  } //getActivity
 
   public int getDay () {
     return dateAndTime.get(Calendar.DATE);
@@ -61,21 +70,36 @@ public class Entry {
 
   public String getTerrain() {
     return null;
-  }
+  }//getTerrain
+  public void setTerrain(String t) {
+    this.terrain = t;
+  }//setTerrain
 
   public String getTempo() {
     return null;
-  }
+  }//getTempo
+  public void setTempo(String t) {
+    this.tempo = t;
+  }//setTempo
 
   public int getRepetitions(){
     return 0;
-  }
+  }//getRepetitions
+  public void setRepetitions(int r) {
+    this.repetitions = r;
+  }//setRepetitions
 
   public int getRecovery(){
     return 0;
-  }
+  }//getRecovery
+  public void setRecovery(int r) {
+    this.recovery = r;
+  }//setRecovery
 
   public String getWhere(){
     return null;
-  }
+  }//getWhere
+  public void setWhere(String w) {
+    this.where = w;
+  }//setWhere
 } // Entry
